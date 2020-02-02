25572b24a779fae79ee5cb2469133afe3fdab7ee
Jiaxi Zhu <jiaxizhu@Jiaxis-MacBook-Pro.local>
2020-02-01 18:31:14 -0800

    Authentication

--- Insta/views.py
+++ Insta/views.py
@@ -3,6 +3,9 @@ from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse, reverse_lazy
 
 from Insta.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 class HelloDjango(TemplateView):
     template_name='home.html'
@@ -12,15 +15,16 @@ class PostsView(ListView):
     model = Post
     template_name = 'index.html'
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
     template_name = 'post_detail.html'
+    login_url = 'login'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
-
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -31,6 +35,11 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("home")
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
 
 
 
--- InstaDemo/urls.py
+++ InstaDemo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from Insta.views import SignUp
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('insta/', include('Insta.urls'))
+    path('insta/', include('Insta.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/signup/',SignUp.as_view(), name='signup'),
 ]
